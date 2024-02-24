90cc441a08fc228921fa659e3e0a63d9eead1471
c-antin <38494182+c-antin@users.noreply.github.com>
2024-02-24 17:39:36 +0100

    bugs

---
+++ main.tsx
@@ -0,0 +1,32 @@
+import { ComponentChildren } from "preact";
+import render from "preact-render-to-string";
+
+//deno run main.tsx
+
+const Component = (
+  props: { children?: ComponentChildren },
+) => {
+  return <>{props.children}</>;
+};
+
+{
+  const jsx = (
+    <Component>
+      "test"
+      <span>test</span>
+    </Component>
+  );
+  const html = render(jsx);
+  console.log("preact empty bug:", html);
+  console.assert(html==="&quot;test&quot;<span>test</span>");
+}
+{
+  const jsx = (
+    <Component>
+      "test"
+    </Component>
+  );
+  const html = render(jsx);
+  console.log("triple escape bug:", html);
+  console.assert(html==="&quot;test&quot;");
+}
