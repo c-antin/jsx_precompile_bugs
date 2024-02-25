0c0d407820412ce58c2c6099d0f110e33453cb4b
c-antin <38494182+c-antin@users.noreply.github.com>
2024-02-25 12:15:23 +0100

    deno emit

--- main.tsx
+++ main.tsx
@@ -1,7 +1,8 @@
 import { ComponentChildren } from "preact";
 import render from "preact-render-to-string";
+import { transpile } from "deno_emit";
 
-//deno run main.tsx
+//deno run --allow-env --allow-read --allow-write --allow-net=deno.land main.tsx
 
 const Component = (
   props: { children?: ComponentChildren },
@@ -18,7 +19,7 @@ const Component = (
   );
   const html = render(jsx);
   console.log("preact empty bug:", html);
-  console.assert(html==="&quot;test&quot;<span>test</span>");
+  console.assert(html === "&quot;test&quot;<span>test</span>");
 }
 {
   const jsx = (
@@ -28,5 +29,18 @@ const Component = (
   );
   const html = render(jsx);
   console.log("triple escape bug:", html);
-  console.assert(html==="&quot;test&quot;");
+  console.assert(html === "&quot;test&quot;");
+}
+{
+  const url = new URL("./main.tsx", import.meta.url);
+  const result = await transpile(url, {
+    importMap: "./import_map.json",
+    compilerOptions: {
+      jsx: "precompile",
+      jsxImportSource: "preact",
+    },
+  });
+  const code = result.get(url.href);
+  // console.log(code);
+  console.assert(!code?.includes("&amp;quot;test&amp;quot;"));
 }
